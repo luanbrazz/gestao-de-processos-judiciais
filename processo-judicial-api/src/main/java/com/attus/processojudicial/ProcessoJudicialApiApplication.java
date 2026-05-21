a602249c0dae3a663b206494b1af14f76a755970
Luan Braz <luan_silvacpv@hotmail.com>
2026-05-21 18:57:25 -0300

    feat: initial project setup with Spring Boot 3.5.14, OpenFeign, Resilience4j and SpringDoc

---
+++ processo-judicial-api/src/main/java/com/attus/processojudicial/ProcessoJudicialApiApplication.java
@@ -0,0 +1,13 @@
+package com.attus.processojudicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoJudicialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoJudicialApiApplication.class, args);
+	}
+
+}
---
+++ processo-judicial-api/src/test/java/com/attus/processojudicial/ProcessoJudicialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attus.processojudicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoJudicialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
