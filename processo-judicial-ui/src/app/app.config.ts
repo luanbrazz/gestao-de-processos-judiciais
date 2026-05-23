e528374e7aae466573ad851399447fa39774cb6a
Luan Braz <luan_silvacpv@hotmail.com>
2026-05-23 01:37:43 -0300

    feat: add Angular 17 frontend project with Bootstrap, ngx-toastr and ngx-spinner

--- processo-judicial-ui/src/app/app.config.ts
+++ processo-judicial-ui/src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(
+      ToastrModule.forRoot({
+        timeOut: 3000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+      }),
+      NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
+    ),
+  ],
 };
